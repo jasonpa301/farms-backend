96d315f3a3fa05a36b5a4a609c87049b32ef39a6
jasonpa301 <jasonpa301@gmail.com>
2022-01-03 16:01:51 +0200

    added some api get routes

--- routes/api.php
+++ routes/api.php
@@ -7,13 +7,16 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/farms/all', function (Request $request) {
+    return Farm::getFarms();
+});
+
+Route::get('/readings/all', function (Request $request) {
+    return Reading::getReadings();
+});
+
+Route::get('/readings/farm/{farmid}', function ($farmId) {
+    return Reading::getReadingsByFarm($farmId);
 });
